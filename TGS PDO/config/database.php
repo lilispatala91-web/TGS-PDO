30f50a7c96577eb80d83499e3e12a9983373762f
lilispatala91-web <lilispatala91@gmail.com>
2025-10-30 12:05:39 +0800

    Add files via upload

---
+++ TGS PDO/config/database.php
@@ -0,0 +1,21 @@
+<?php
+// Konfigurasi koneksi database
+$host = "localhost";      // Server lokal
+$dbname = "PDO";     // Nama database kamu
+$username = "root";       // Username XAMPP (default: root)
+$password = "Assryy020804@!";           // Password XAMPP (biasanya kosong)
+
+try {
+    // Membuat koneksi menggunakan PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    // Atur mode error agar muncul sebagai exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Jika berhasil
+    echo "Koneksi ke database berhasil!";
+} catch (PDOException $e) {
+    // Jika gagal
+    echo "Koneksi gagal: " . $e->getMessage();
+}
+?>
